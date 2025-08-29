75a1c14332818736ad3f04cd0ed4a91b272f2f83
Vigyan_07 <baralbigyan48@gmail.com>
2025-08-29 17:46:00 +0545

    inside typecasting

---
+++ 03typecasting.py
@@ -0,0 +1,23 @@
+#typecasting --> process of changing data type of variable from one to another . int() bool() str() float()
+name = "bigyan"
+age = 17
+gpa = 3.56
+student = True
+
+#integer to string !
+age = str(age)
+print(type(age))
+age += "bigyan"
+age = age + "baral"
+print(age)
+
+#string to boolean !
+name = bool(name)
+print(name)
+caste = ""
+caste = bool(caste)
+print(caste)
+
+#string to integer
+name = int(name)
+print(name)
