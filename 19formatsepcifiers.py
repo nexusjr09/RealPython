b9f6c266b7a0bcf7eb7cec61c36f56817ae4aa3f
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-11 20:00:05 +0545

    updates

--- 19formatsepcifiers.py
+++ 19formatsepcifiers.py
@@ -1,5 +1,5 @@
 #format specifiers
-price1 = 120.99
+price1 = 120.99888
 price2 = 45.67
 price3 = 67.89
 
@@ -18,3 +18,13 @@ print(f"Data 3 is : {data3:.1f}")
 
 #with this you can create your space for output
 print(f"Checking for Space : {price1:10}") #----> Creates 10 spaces for output
+
+#PADDING INFRONT OF NUMBER : 
+print(f"Price 1 is {price1:010}") #---> In output now 0 will be padded to fulfill 10 spaces 
+print(f"price 1 is {price1:08}")
+
+#JUSTIFIERS (LEFT OR RIGHT MEANING THE SPACES FOR OUTPUT WILL BE DISPLAYED ON RIGHT OR LEFT )
+
+pro = 67.89
+print(f"pro is : {pro:<10}") #main data will be displayed on left and spaces on right 
+print(f"pro is {pro:>10}")
