44a9c3b70f052f616ad498746f8b0092e7f084d4
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-12 21:19:46 +0545

    while loops in python final one !

--- 20whileloops.py
+++ 20whileloops.py
@@ -7,9 +7,15 @@ while name == "":
 print(f"your name is {name}")
 
 age = int(input("Enter your age : "))
-while age<0:
-    print("Age cannot Be Negative , Please Enter valid Age : ")
+while age < 0 :
+    print("Age cannot Be Negative/empty , Please Enter valid Age : ")
     age = int(input("Enter your age: "))
-
 print(f"You are {age} years old")
 
+#ANOTHER EXAMPLE : 
+food = input("Enter your favourite food : ")
+while not food == "q" : 
+    print(f"Your fav food is {food} !")
+    food = input("Enter another fav food (q to quit ! )")
+
+print("Bye bye ")
