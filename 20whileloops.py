f66630b301b290d2071b0cfca9c7ff1bd555ca02
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-12 21:04:39 +0545

    Changes

--- 20whileloops.py
+++ 20whileloops.py
@@ -5,3 +5,11 @@ while name == "":
     name = input("Enter your name : ") #---> IF THIS LINE IS NOT ADDDED THEN YOU WILL ENTER IN A LOOP THAT WILL ONLY DISPLAY THE PRINT STATEMENT INFINITE TIMES
 
 print(f"your name is {name}")
+
+age = int(input("Enter your age : "))
+while age<0:
+    print("Age cannot Be Negative , Please Enter valid Age : ")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")
+
