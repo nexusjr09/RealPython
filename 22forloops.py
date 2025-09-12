50159702e5561af38db4a4cb7640af81cebbbfe9
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-12 22:17:25 +0545

    forloops 01

---
+++ 22forloops.py
@@ -0,0 +1,13 @@
+#FOR LOOPS ----> executes a block of code for fixed number of times . 
+
+for x in range(1,11):
+    print(x)
+
+print("Ok done ")
+
+for x in reversed(range(1,7,2)):
+    print(x)
+
+credit = int(input("Enter your credit card no : "))
+for x in credit:
+    print(x)
