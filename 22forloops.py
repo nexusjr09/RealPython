dcaa941b008e57f2be80c1c8b9118aaa4f28b9e2
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-13 22:16:59 +0545

    forloops

--- 22forloops.py
+++ 22forloops.py
@@ -8,7 +8,12 @@ print("Ok done ")
 for x in reversed(range(1,7,2)):
     print(x)
 
-credit = int(input("Enter your credit card no : "))
+credit = input("Enter your credit card no : ")
 for x in credit:
     print(x)
 
+for x in range(1,11):
+  if x == 13:
+      continue
+  else:
+      print(x)
