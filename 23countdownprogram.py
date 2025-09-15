aa3fde1e5e6d29ad7cdb0c6705f26cdc49923da2
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-15 20:36:12 +0545

    seconds

--- 23countdownprogram.py
+++ 23countdownprogram.py
@@ -2,7 +2,8 @@ import time
 
 my_time = int(input("Enter the time in seconds : "))
 for x in (range (my_time,0,-1)):
-    print(x)
+    seconds = x % 60
+    print(f"00:00:{seconds:02}")
 time.sleep(1)
 print("Work done ! ")
 
