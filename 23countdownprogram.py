ce7bd7480da4a26706a868acfa7022cf06c32535
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-19 11:05:26 +0545

    changes

--- 23countdownprogram.py
+++ 23countdownprogram.py
@@ -1,11 +1,1 @@
-import time 
-
-my_time = int(input("Enter the time in seconds : "))
-for x in (range (my_time,0,-1)):
-    seconds = x % 60
-    print(f"00:00:{seconds:02}")
-
-time.sleep(1)
-print("Work done ! ")
-
-#DIDNOT UNDERSTAND 
+#countdown processs
