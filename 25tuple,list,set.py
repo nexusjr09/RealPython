1d64f168a73d3f30ed92153c80e07ecca9fbb98e
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-23 20:53:05 +0545

    Major updates

--- 25tuple,list,set.py
+++ 25tuple,list,set.py
@@ -11,3 +11,27 @@ print(fruits[::-1])
 
 #checking if value is within the list or not .
 print("apple" in fruits)
+
+#changing values from the list : 
+fruits[0] = "pineapple"
+print(fruits)
+
+#adding values to the list :
+fruits.append("jomsomapple")
+print(fruits)
+
+#removing values from the list:
+fruits.remove("banana")
+print(fruits)
+
+#adding values to the exact position :
+fruits.insert(0,"coconut")
+print(fruits)
+
+#sorting values alphabetically : 
+fruits.sort()
+# no print statement needed while performing in this format 
+
+#reversing  the list 
+fruits.reverse() #MAIN THING TO UNDERSTAND HERE IS : IT WILL ONLY REVERSE THE VALUE OF FRUITS THAT WAS DISPLAYED BEFORE USING THIS FUNCTION . (JUST ABOVE VALUE)
+
