9092eb798cd73d0a5d9721f140e2159b6fbbd5fe
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-25 19:44:05 +0545

    2D collection with numpad all explained

--- 27_2Dcollection.py
+++ 27_2Dcollection.py
@@ -14,11 +14,18 @@ snacks =  [["lays", "burger","pizza"],
 
 #print(snacks[2][1])
 
-for snack in snacks:
-    for sna in snack:
+for snack in snacks: #it will represent single iteration at a time . and then after completing it , it will move to second iteration /second line . 
+    for sna in snack:  #sna here represents whole word  not single letter . 
         print(sna,end = " ")
-        print() #It means: "after finishing one round of the inner loop, move to a new line not every alphabet  will be print in new line only after completing full word 
- #l
- #a
- #y
- #s
+    print() #It means: "after finishing one round of the inner loop, move to a new line not every alphabet  will be print in new line only after completing full word 
+
+#NUMPAD 
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*",0,"#"))
+for num in num_pad:
+    for n in num:
+        print(n , end = " ")
+    print()
