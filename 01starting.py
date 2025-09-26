610c0b10e71ef54c768a40f2f21cb393047ed84d
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-26 19:58:48 +0545

    changes {

--- 01starting.py
+++ 01starting.py
@@ -1,2 +1,7 @@
 print("starting of Pyhton")
 #starting point
+questions = (" What is my Name : ",
+             "what is my age : ",
+             "who tf I am you think biatch ")
+
+answers = ("A. Walter White", "B. Nexus Code ")
