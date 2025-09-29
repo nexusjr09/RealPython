05f6e414428f199555626ef5e9e91f216256e677
Vigyan_07 <baralbigyan48@gmail.com>
2025-09-29 09:03:03 +0545

    dict

---
+++ 29dictionary.py
@@ -0,0 +1,22 @@
+#dictionary 
+
+capitals = {"USA":"Washington DC",
+        "Nepal": "Kathmandu",
+        "China":"Bejing",
+        "India": "NewDelhi"}
+#print(dir(capitals))
+print(capitals.get("Nepal"))
+print(capitals.get('USA'))
+
+if capitals.get("Japan"):
+    print("Capital Exists ! ")
+else:
+    print("Capital doesn't exists")
+
+capitals.update({"Japan":"Tokyo"}) # USED TO UPDATE VALUES IN CAPITALS 
+print(capitals)
+capitals.pop("Nepal")
+print(capitals) #removes the value from the dictionary ! 
+#capitals.popitem() ------> removes the latest value added . 
+#capitals.clear()-----> clears all the value inside dictionary !
+    
